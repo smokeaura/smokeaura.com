3b4bc206703f3e08c703ae46c0830c012b825cc5
zeekay <zk@monoid.io>
2016-03-04 16:03:43 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# smokeaura.com
+
+## Dependencies
+```
+$ npm install && npm install -g gulp
+```
+
+## Build
+```
+$ gulp build
+```
+
+## Running the development server
+```
+$ gulp
+```
---
+++ js/functions.js
@@ -0,0 +1,41 @@
+(function($, window, document, undefined) {
+	var $win = $(window);
+	var $doc = $(document);
+
+	$doc.ready(function() {
+		$('.slider-primary .slides').owlCarousel({
+			items: 1,
+			nav: true,
+			dots: true,
+		})
+
+		$('.slider-timeline .slides').owlCarousel({
+			items: 1,
+			nav: true,
+			dots: true,
+			startPosition: 5,
+		})
+
+		var tryUpdateClasses = function(){
+		  if ($('.ico-prev')[0]) {
+		    return
+		  }
+
+          $('.slider .owl-prev').addClass('ico-prev')
+          $('.slider .owl-next').addClass('ico-next')
+
+          requestAnimationFrame(tryUpdateClasses)
+		}
+
+        requestAnimationFrame(tryUpdateClasses)
+
+		//Mobile Menu
+		$('.nav-trigger').on('click', function (event) {
+			event.preventDefault();
+
+			$(this).toggleClass('active');
+
+			$('.nav').toggleClass('visible');
+		});
+	});
+})(jQuery, window, document);
